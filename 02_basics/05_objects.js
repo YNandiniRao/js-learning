79cb0ed51bf63a3f13dd414dbfaf105285dd5798
YNandiniRao <ynandinirao02@gmail.com>
2025-12-18 14:45:38 +0000

    objects

---
+++ 02_basics/05_objects.js
@@ -0,0 +1,27 @@
+const course = {
+    coursename: "JS in hindi",
+    price: "999",
+    courseInstructor: "abcd"
+}
+
+//course.courseInstructor
+
+const {courseInstructor: instructor} = course
+
+//console.log(courseInstructor);
+console.log(instructor);
+
+
+/*
+{
+    "name": "nandini",
+    "coursename": "js in hindi",
+    "price": "free"
+}
+*/
+
+[
+    {},
+    {},
+    {}
+]
